8b064d68c289df491fe025bfbdaa123251009fe2
alexporters <lyhewyjd78333@outlook.com>
2025-11-24 14:26:48 +0800

    deploy js

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedPrismLock = await deploy("PrismLock", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`PrismLock contract: `, deployedPrismLock.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_prism_lock"; // id required to prevent reexecution
+func.tags = ["PrismLock"];
